e532c103ca7441f07838adbace660688552c5589
Shrishi2000 <65821403+ss-shrishi2000@users.noreply.github.com>
2021-05-30 14:50:01 +0530

    Update CityMarker.java

--- SeismicDataVisualization/src/module4/CityMarker.java
+++ SeismicDataVisualization/src/module4/CityMarker.java
@@ -9,7 +9,7 @@ import processing.core.PGraphics;
 /** Implements a visual marker for cities on an earthquake map
  * 
  * @author UC San Diego Intermediate Software Development MOOC team
- * @author Sudharaka Palamakumbura
+ * @author Shrishti Singh
  *
  */
 public class CityMarker extends SimplePointMarker {
