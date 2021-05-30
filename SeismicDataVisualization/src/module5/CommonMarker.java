04436e8468c034a8ee8489698f41cac4ac946f06
Shrishi2000 <65821403+ss-shrishi2000@users.noreply.github.com>
2021-05-30 14:53:25 +0530

    Update CommonMarker.java

--- SeismicDataVisualization/src/module5/CommonMarker.java
+++ SeismicDataVisualization/src/module5/CommonMarker.java
@@ -7,7 +7,7 @@ import processing.core.PGraphics;
 /** Implements a common marker for cities and earthquakes on an earthquake map
  * 
  * @author UC San Diego Intermediate Software Development MOOC team
- * @author Sudharaka Palamakumbura
+ * @author Shrishti Singh
  *
  */
 public abstract class CommonMarker extends SimplePointMarker {
